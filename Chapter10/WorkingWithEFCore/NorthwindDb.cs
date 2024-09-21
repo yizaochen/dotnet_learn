6920d776469756b05d5ba37a612dc2b13f314a7b
yizaochen <yizaochen@gmail.com>
2024-09-21 19:56:19 +0800

    EF Learn

---
+++ Chapter10/WorkingWithEFCore/NorthwindDb.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+namespace Northwind.EntityModels;
+
+public class NorthwindDb : DbContext
+{
+    // These two properties map to tables in the database.
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string databaseFile = "Northwind.db";
+        string path = Path.Combine(Environment.CurrentDirectory, databaseFile);
+        string connecionString = $"Data Source={path}";
+        WriteLine($"Connection: {connecionString}");
+        optionsBuilder.UseSqlite(connecionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Example of using Fluent API instead of attributes
+        // to limit the length of a category name to 15.
+        modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired() // NOT NULL
+            .HasMaxLength(15);
+        
+        // Some SQLite-specific configuration.
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            // To "fix" the lack of decimal support in SQLite.
+            modelBuilder.Entity<Product>()
+                .Property(product => product.Cost)
+                .HasConversion<double>();
+        }
+
+        // A global filter to remove discontinued products.
+        // modelBuilder.Entity<Product>().HasQueryFilter(p => !p.Discontinued);
+    }
+
+}
---
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -0,0 +1,7 @@
+﻿using Northwind.EntityModels;
+// using NorthwindDb db = new();
+// WriteLine($"Provider: {db.Database.ProviderName}");
+
+ConfigureConsole();
+// QueryingCategories();
+FilteredIncludes();
